7cbd56254ec1aefd68c2aff83d4db0c957a457e4
horiuchit71 <takuya.horiuchi@g.softbank.co.jp>
2019-03-10 05:01:47 +0900

    make project

---
+++ src/main/java/recipesystem/TrackExaminationApplication.java
@@ -0,0 +1,13 @@
+package recipesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackExaminationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackExaminationApplication.class, args);
+	}
+
+}
