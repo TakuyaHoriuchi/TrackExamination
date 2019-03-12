21faabd21fdc2dfff34117763d565bbe364a6345
horiuchit71 <takuya.horiuchi@g.softbank.co.jp>
2019-03-13 04:10:09 +0900

    TODO記載

--- src/main/java/recipesystem/TrackExaminationApplication.java
+++ src/main/java/recipesystem/TrackExaminationApplication.java
@@ -11,3 +11,36 @@ public class TrackExaminationApplication {
 	}
 
 }
+// =============CONTROLLER===============
+// TODO Read複数件取得実装修正
+// TODO CREATEのテスト実装
+// TODO CREATEの実装修正
+// TODO UPDATEのテスト実装
+// TODO UPDATEの実装修正
+// TODO DELETEのテスト実装
+// TODO DELETEの実装修正
+// =============CONTROLLER===============
+// =============SERVICE===============
+// TODO 1件Readのテスト実装
+// TODO 1件Readの実装修正
+// TODO 全件Readのテスト実装
+// TODO 全件Readの実装修正
+// TODO CREATEのテスト実装
+// TODO CREATEの実装修正
+// TODO UPDATEのテスト実装
+// TODO UPDATEの実装修正
+// TODO DELETEのテスト実装
+// TODO DELETEの実装修正
+//=============SERVICE===============
+// TODO DBをセットしてDEPENDENCYを注入
+// TODO FORMAT整理
+// TODO ERRORレベルのLOGの埋込
+// TODO 問題記載以外の異常系実装
+// TODO INFOレベルのLOGの埋込
+
+
+
+
+
+
+
