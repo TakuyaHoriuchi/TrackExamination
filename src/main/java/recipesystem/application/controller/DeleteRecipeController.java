da96dd48bbda43f64c584b1ef89d5f08f62612a0
horiuchit71 <takuya.horiuchi@g.softbank.co.jp>
2019-03-16 06:12:37 +0900

    正常系実装修正

--- src/main/java/recipesystem/application/controller/DeleteRecipeController.java
+++ src/main/java/recipesystem/application/controller/DeleteRecipeController.java
@@ -1,10 +1,16 @@
 package recipesystem.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import recipesystem.application.payload.ResponseRecipe;
+import recipesystem.domain.service.DeleteRecipeService;
 
 public class DeleteRecipeController {
-
+  @Autowired
+  DeleteRecipeService service;
+  
   public ResponseRecipe deleteRecipe(int i) {
+    service.delete(i);
     ResponseRecipe response = new ResponseRecipe();
     response.setMessage("Recipe successfully removed!");
     return response;
